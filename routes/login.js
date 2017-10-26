ced5c80e7e31af8d624f58c19b1b960265d2bc8e
wankyukr <wankyu19@gmail.com>
2017-10-26 17:17:51 +0900

    renew login controller

--- routes/login.js
+++ routes/login.js
@@ -1,24 +1,34 @@
 const url = require('url');
 const express = require('express');
 const router = express.Router();
+const readlineSync = require('readline-sync');
+const getArgv = require('../lib/getArgv');
+
+let user_name = getArgv('-u', '--user') || readlineSync.question('Name: ', {});
+let user_passwd = readlineSync.questionNewPassword('Password: ', {
+	mask: '',
+	min: 6,
+	confirmMessage: 'Confirm Password: ',
+	limitMessage: 'Not available password. Try another.',
+});
+console.log('Ok');
 
 router.get('/', function(req, res, next) {
+    let referer_path = url.parse(req.headers.referer || '/').pathname;
     if(req.session.isLoggedIn) {
         req.session.isLoggedIn = false;
         req.session.save(() => {
-            res.redirect('/');
+            res.redirect(referer_path);
         });
     } else {
-        let name = req.session.username || 'none';
-        let checkpw = req.session.isLoggedIn || false;
         console.log('id:::', req.sessionID);
-        res.render('login.jsx', {test: name, checkpw: checkpw, referer: url.parse(req.headers.referer).pathname});
+        res.render('login.jsx', {referer: referer_path});
     }
 });
 
 router.post('/', function(req, res, next) {
     req.session.username = req.body.username;
-    req.session.isLoggedIn = ('1234' == req.body.password);
+    req.session.isLoggedIn = (user_passwd == req.body.password && user_name == req.body.username);
     console.log('id:::', req.sessionID);
     req.session.save(() => {
         //console.log('login:::', req.session);
--- views/login.jsx
+++ views/login.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 
-const Result = (props) => {
-    return (
-        <div>
-            <p>{Date.now()}</p>
-            <p>Name: {props.test}</p>
-            <p>Password Correct: {props.checkpw.toString()}</p>
-        </div>
-   );
-};
-
 const Form = (props) => {
     return (
         <form action="./login" method="post">
-            <input name="username" type="text" placeholder="Username" />
-            <input name="password" type="password" placeholder="Password" />
-            <input name="referer" type="hidden" value={props.referer} />
-            <input type="submit" value="Submit" />
+            <fieldset className="login_fieldset">
+                <legend>Login</legend>
+                <input name="username" type="text" placeholder="Username" />
+                <input name="password" type="password" placeholder="Password" />
+                <input name="referer" type="hidden" value={props.referer} />
+                <input type="submit" value="Submit" />
+            </fieldset>
         </form>
     );
 };
@@ -30,8 +23,6 @@ class Login extends React.Component {
                     <link rel="stylesheet" href="/css/style.css" />
                 </head>
                 <body>
-                    <h5>Login</h5>
-                    <Result {...this.props} />
                     <Form referer={this.props.referer} />
                 </body>
                 </html>
